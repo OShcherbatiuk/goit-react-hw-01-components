db5d8fddde7819cd7d72e357870a0bf1d0213256
Oleksii Shcherbatiuk <oleksii.shcherbatiuk@gmail.com>
2021-04-19 17:05:13 +0300

    upd

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -14,12 +14,16 @@ const Statistics = ({ title, stats }) => (
     </section>
 );
 
+Statistics.defaultProps = {
+  title: '',
+};
+
 Statistics.propTypes = {
     title: PropTypes.string,
     stats:PropTypes.arrayOf(PropTypes.shape({
         id: PropTypes.string.isRequired,
-        label: PropTypes.string,
-        percentage: PropTypes.number,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired,
   })),
 };
 
